5f9b53438e4eba3ff8864e00e26f00527f971402
Justyna Wareliś <justyna.warelis@edu.uekat.pl>
2024-02-29 18:30:07 +0100

    add github actions

--- main.py
+++ main.py
@@ -4,16 +4,19 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
+
 class HelloWorld(Resource):
     def get(self):
         return {'hello': 'world'}
 
-#class HelloWorld2(Resource):
+
+# class HelloWorld2(Resource):
 #   def get(self):
 #       return {'hello': 'world2'}
 
-api.add_resource(HelloWorld, '/test') #endpoint pierwszy
-#api.add_resource(HelloWorld2, '/test2') endpoint drugi
+# endpoint pierwszy
+api.add_resource(HelloWorld, '/test')
+# api.add_resource(HelloWorld2, '/test2') endpoint drugi
 
 if __name__ == '__main__':
     app.run(debug=True)
